8a0ee03b57dbf7cc1dc646ac860d5ee7b65f0b39
DanielGiron20 <girondaniel388@gmail.com>
2024-12-01 18:18:44 -0600

    Cambios en web.py para obtener titulo y contenido

--- Pagina Web - Backend/Scrapping/web.py
+++ Pagina Web - Backend/Scrapping/web.py
@@ -5,21 +5,26 @@ from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 CORS(app) 
-@app.route('/Web', methods=['get'])
+@app.route('/Web', methods=['POST'])
 
 def api():
     try:
          data = request.json
          url = data.get('url')
-         response = requests.get(url)
-         response.raise_for_status()
-
+         headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'}
+         response = requests.get(url, headers=headers)
+         response.raise_for_status()  
          soup = BeautifulSoup(response.text, 'html.parser')
-         content = soup.get_text()
+         title = soup.find('h1').get_text(strip=True)
 
-         return jsonify({'content': content})
+         paragraphs = soup.find_all('p')
+         content = ' '.join(p.get_text(strip=True) for p in paragraphs)
+
+         return jsonify({'title': title, 'content': content})
     except Exception as e:
         return jsonify({'error': str(e)}), 500
     
 if __name__ == '__main__':
     app.run(debug=True)
+
+
